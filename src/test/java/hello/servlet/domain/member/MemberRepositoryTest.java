70d6c6f99ba53602424167d6cc03a66b18421d9b
SUN <siksik2782259@gmail.com>
2024-04-12 10:29:52 +0900

    섹션3. 서블릿, JSP, MVC 패턴

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,47 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+    // 싱글톤이니까 new MemberRepository하면 안된다!
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        // 테스트 초기화 용도
+        memberRepository.clearStore();
+    }
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member savedMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2); // 데이터가 맞는지 확인
+    }
+}
